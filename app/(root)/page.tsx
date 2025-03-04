f7ec7ba03a4f8b462aceda247e8e8f7f20cd65a1
Mehmet <mehmetKmrc1109@gmail.com>
2025-03-04 15:08:19 +0300

    feat:Project structure created

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
+
+const HomePage = async () => {
+  
+  return (<> <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} /> </>);
+}
+ 
+export default HomePage;
