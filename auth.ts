b7ec229e3546dc456e634b5e1018986e2c61f84d
Mehmet <mehmetKmrc1109@gmail.com>
2025-04-03 22:18:46 +0300

    fix: Size limitation error fixed, divided auth into 2 files

--- auth.ts
+++ auth.ts
@@ -3,7 +3,10 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/db/prisma';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compareSync } from 'bcrypt-ts-edge';
-import type { NextAuthConfig } from 'next-auth';
+import { authConfig } from './auth.config';
+import { cookies } from 'next/headers';
+
+
 
 export const config = {
     pages: {
@@ -11,7 +14,7 @@ export const config = {
         error: '/sign-in',
     },
     session: {
-        strategy: 'jwt',
+        strategy: 'jwt' as const,
         maxAge: 30*24*60*60 //30 days
     },
     adapter: PrismaAdapter(prisma),
@@ -50,38 +53,7 @@ export const config = {
                 },
             }),
     ],
-    callbacks: {
-        async session({ session, user, trigger, token}: any) {
-            //Set the user ID from the token
-            session.user.id = token.sub;
-            session.user.role = token.role;
-            session.user.name = token.name;
-
-            //If there is an update, set the user name
-            if(trigger === 'update') {
-                session.user.name = user.name;
-            }
-            return session;
-        },
-        async jwt({token, user, trigger, session}: any) {
-            //Assign user fields to token
-            if (user){
-                token.role = user.role;
-
-                //If user has no name then use the email
-                if (user.name === 'NO_NAME'){
-                    token.name = user.email!.split('@')[0];
-
-                    //Update database to reflect the token name
-                    await prisma.user.update({
-                        where: { id: user.id },
-                        data: { name: token.name },
-                    });
-                }
-            }
-            return token;
-        }
-    },
-}satisfies NextAuthConfig;
+    ...authConfig.callbacks,
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
